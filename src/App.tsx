1e3e44f70118d5b49fc70f15602272789da6d865
sah.arjun <sah.arjun07@gmail.com>
2025-04-08 22:14:13 +0545

    optimized root route and unmatched url

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ const App: React.FC = () => {
     <div style={{display: "flex", height: "100vh", width: "100vw"}}>
       <div style={{flex: 1, overflow: "auto", padding: "1rem", borderRight: "1px solid #ccc", boxSizing: "border-box"}}>
         <Routes>
-          <Route path="/" element={<Products/>}/>
+          <Route path="/" element={<h2>Please select a product from the list.</h2>}/>
           <Route path="/product/:id" element={<ProductById/>}/>
-          <Route path="*" element={<h2>Please select a product from the list.</h2>} />
+          <Route path="*" element={<h2>Not Found</h2>} />
         </Routes>
       </div>
       <div style={{flex: 1, overflow: "auto", padding: "1rem", boxSizing: "border-box"}}>
